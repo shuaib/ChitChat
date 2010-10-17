02b6c09beaee1b5e65e5773b2729326f126de061
Shuaib <shuaib@bytehood.com>
2010-10-17 20:43:13 +0500

    Adds index view to views.py

--- chitchat/chatroom/views.py
+++ chitchat/chatroom/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+
+
+def index(request):
+	return HttpRequest("Index page")
