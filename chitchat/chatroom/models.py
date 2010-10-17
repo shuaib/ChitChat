37e97f85409ce7f7a7c5e678d529a4a47a2629ce
Shuaib <shuaib@bytehood.com>
2010-10-17 20:46:27 +0500

    Adds Message model to models.py

--- chitchat/chatroom/models.py
+++ chitchat/chatroom/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+	message = models.CharField(max_length=240)
+	timestamp = models.DateTimeField(auto_now=True)
+	sender_name = models.CharField(max_length=20)
+
+	def __unicode__(self):
+		return self.message
+
+	class Meta:
+		ordering = ['-id']
