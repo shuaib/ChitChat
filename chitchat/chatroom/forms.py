14976254726c50af148ce2ae2a29319545efe8d8
Shuaib <shuaib@bytehood.com>
2010-10-17 21:26:07 +0500

    Changes message to text in form. Modifies index.html

--- chitchat/chatroom/forms.py
+++ chitchat/chatroom/forms.py
@@ -4,9 +4,9 @@ from chatroom.models import Message
 
 class MessageForm(forms.ModelForm):
 
-	message = forms.CharField(max_length=240, required=True)
+	text = forms.CharField(max_length=240, required=True)
 	sender = forms.CharField(max_length=20, required=False)
 
 	class Meta:
 		model = Message
-		exclue = ('timestamp')
+		exclue = ('timestamp', 'sender')
