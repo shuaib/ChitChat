6aad58f9fb2a6927d25d1348f7eabf6b18a2bcc2
Shuaib <shuaib@bytehood.com>
2010-10-17 20:35:42 +0500

    Adds root url to urls.py. Sets debut mode static files view

--- chitchat/urls.py
+++ chitchat/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
-
+from django.conf import settings
+import os.path
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +15,13 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
+	(r'^/', include('chitchat.chatroom.urls')),
 )
+
+if settings.DEBUG:
+	urlpatterns += patterns('',
+			(r'^static/(?<path>.*)$', 'django.views.static.serve',
+				{'document_root':os.path.join(os.path.dirname(__file__),
+					"static")}
+				),
+			)
